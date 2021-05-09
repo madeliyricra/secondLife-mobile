534db4746093f0e5035f2b0becb2be29c5a2fafd
madeliyricra <madeliyricra@gmail.com>
2021-05-08 23:35:36 -0500

    vista de inicio de la app

---
+++ app/src/main/java/com/store/secondlife/view/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,11 @@
+package com.store.secondlife.view.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
