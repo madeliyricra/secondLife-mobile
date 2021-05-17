314e1464593e8203f5c09061eacc88ee0a37358c
madeliyricra <madeliyricra@gmail.com>
2021-05-17 15:44:57 -0500

    item de compra y venta lista

--- app/src/main/java/com/store/secondlife/view/ui/fragments/DetailPurchaseDialogFragment.kt
+++ app/src/main/java/com/store/secondlife/view/ui/fragments/DetailPurchaseDialogFragment.kt
@@ -35,7 +35,7 @@ class DetailPurchaseDialogFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_purchase, container, false)
+        return inflater.inflate(R.layout.detail_purchase_dialog, container, false)
     }
 
     companion object {
