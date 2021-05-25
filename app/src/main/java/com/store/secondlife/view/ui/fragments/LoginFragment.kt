182502a9056fe639620bfcb9d08fd6df04a76194
Rodolfo Peralta <peraltarodolfo4s@gmail.com>
2021-05-25 16:09:08 -0500

    Google auth +login con error

--- app/src/main/java/com/store/secondlife/view/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/store/secondlife/view/ui/fragments/LoginFragment.kt
@@ -1,28 +1,35 @@
 package com.store.secondlife.view.ui.fragments
 
 import android.content.ContentValues.TAG
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.store.secondlife.MainActivity
 import com.store.secondlife.R
 
+
 class LoginFragment : Fragment(), View.OnClickListener {
     lateinit var btnSignIn: Button
     lateinit var btnSignUp: Button
+    lateinit var btnGoogle: Button
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     lateinit var txtusu: EditText
     lateinit var txtpass: EditText
@@ -31,11 +38,19 @@ class LoginFragment : Fragment(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
         // Initialize Firebase Auth
         auth = Firebase.auth
 
-        if (auth.currentUser != null) {
 
+        if (auth.currentUser != null) {
             val user = auth.currentUser
             val intent: Intent = Intent(requireContext(), MainActivity::class.java)
             requireActivity().startActivity(intent)
@@ -43,6 +58,53 @@ class LoginFragment : Fragment(), View.OnClickListener {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+
+    }
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -58,11 +120,12 @@ class LoginFragment : Fragment(), View.OnClickListener {
         btnSignIn.setOnClickListener(this)
         btnSignUp = view.findViewById(R.id.btnSignUp)
         btnSignUp.setOnClickListener(this)
+        btnGoogle=view.findViewById(R.id.btnGoogle)
+        btnGoogle.setOnClickListener(this)
 
         txtusu = view.findViewById(R.id.txtusu)
         txtpass = view.findViewById(R.id.txtpass)
     }
-
 
     override fun onClick(p0: View?) {
 
@@ -73,20 +136,43 @@ class LoginFragment : Fragment(), View.OnClickListener {
                 .addOnCompleteListener(requireActivity()) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithEmail:success")
+                        Log.d(TAG2, "signInWithEmail:success")
                         val user = auth.currentUser
                         updateUI(user)
-                        findNavController().navigate(R.id.navProfileFragment)
+                        findNavController().navigate(R.id.action_loginFragment_to_navProfileFragment)
                     } else {
-                        
+                        Log.w(TAG2, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(context, "No se puede realizar esta acción.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+
                     }
                 }
         } else if (btnSignUp == p0) {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
-    }
-    private fun updateUI(user: FirebaseUser?) {
 
+        if (btnGoogle ==p0){
+            signIn()
+            findNavController().navigate(R.id.action_loginFragment_to_navProfileFragment)
+        }else {
+            Toast.makeText(context, "Authentication failed.",
+                Toast.LENGTH_SHORT).show()
+            updateUI(null)
+
+        }
+    }
+        private fun reload() {
+
+        }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+        private const val TAG2 = "EmailPassword"
     }
 
 }
--- app/src/main/java/com/store/secondlife/view/ui/fragments/RegisterFragment.kt
+++ app/src/main/java/com/store/secondlife/view/ui/fragments/RegisterFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -18,16 +19,16 @@ import com.google.firebase.ktx.Firebase
 import com.store.secondlife.MainActivity
 import com.store.secondlife.R
 
-class RegisterFragment : Fragment(),View.OnClickListener {
-    lateinit var btnok:Button
-    lateinit var btncancel:Button
+class RegisterFragment : Fragment(), View.OnClickListener {
+    lateinit var btnok: Button
+    lateinit var btncancel: Button
 
-    lateinit var txtEmail:EditText
-    lateinit var txtUser:EditText
-    lateinit var txtDNI:EditText
-    lateinit var txtFirstName:EditText
-    lateinit var txtLastName:EditText
-    lateinit var txtPassword:EditText
+    lateinit var txtEmail: EditText
+    lateinit var txtUser: EditText
+    lateinit var txtDNI: EditText
+    lateinit var txtFirstName: EditText
+    lateinit var txtLastName: EditText
+    lateinit var txtPassword: EditText
     private lateinit var auth: FirebaseAuth
 
 
@@ -46,50 +47,56 @@ class RegisterFragment : Fragment(),View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        txtEmail=view.findViewById(R.id.txtEmail)
-        txtUser=view.findViewById(R.id.txtUser)
-        txtDNI=view.findViewById(R.id.txtDNI)
-        txtFirstName=view.findViewById(R.id.txtFirstName)
-        txtLastName=view.findViewById(R.id.txtLastName)
-        txtPassword=view.findViewById(R.id.txtPassword)
+        txtEmail = view.findViewById(R.id.txtEmail)
+        txtUser = view.findViewById(R.id.txtUser)
+        txtDNI = view.findViewById(R.id.txtDNI)
+        txtFirstName = view.findViewById(R.id.txtFirstName)
+        txtLastName = view.findViewById(R.id.txtLastName)
+        txtPassword = view.findViewById(R.id.txtPassword)
 
-        btnok=view.findViewById(R.id.btnok)
-        btncancel=view.findViewById(R.id.btncancel)
+        btnok = view.findViewById(R.id.btnok)
+        btncancel = view.findViewById(R.id.btncancel)
         btnok.setOnClickListener(this)
-        btncancel.setOnClickListener (this)
+        btncancel.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
 
-        if(p0==btnok){
-            val email:String=txtEmail.text.toString()
-            val user:String=txtUser.text.toString()
-            val dni: String=txtDNI.text.toString()
-            val name:String=txtFirstName.text.toString()
-            val lastn:String=txtLastName.text.toString()
-            val contra:String=txtPassword.text.toString()
+        if (p0 == btnok) {
+            val email: String = txtEmail.text.toString()
+            val contra: String = txtPassword.text.toString()
 
             auth.createUserWithEmailAndPassword(email, contra)
                 .addOnCompleteListener(requireActivity()) { task ->
                     if (task.isSuccessful) {
-                        findNavController().navigate(R.id.action_registerFragment_to_passwordFragment)
+                        Log.d(TAG, "createUserWithEmail:success")
+                        Toast.makeText(
+                            context, "Usuario creado exitosamente.",
+                            Toast.LENGTH_SHORT).show()
                         val user = auth.currentUser
                         updateUI(user)
                         /*val intent:Intent=Intent(requireContext(),MainActivity::class.java)
                         requireActivity().startActivity(intent)
                         requireActivity().finish()*/
-
+                        findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
                     } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            context, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
                     }
                 }
-
-        }else if(p0==btncancel){
+        } else if (p0 == btncancel) {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
-
     }
-    private fun updateUI(user: FirebaseUser?) {
 
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 
 }
